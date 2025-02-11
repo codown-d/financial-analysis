54c8100f8b2e68f3d1b4c50a9b7c27267311f6c8
zhang he <he.zhang@neuetech.cn>
2025-02-11 11:12:37 +0800

    feat:修改  baseurl 逻辑

--- data-system/app.py
+++ data-system/app.py
@@ -11,17 +11,21 @@ from utils.file_utils import check_data_exists, save_code_to_file
 # # 示例用法
 
 def main():
-    folder_name = "2024-02-07"
+    folder_name = "2024-02-08"
     file_name = "code-data.csv"
-    code_name = "000001"
+    code_name = "sz000001"
     # 调用检查方法
-    data_exists = check_data_exists(folder_name, file_name, code_name)
-    if not data_exists:
-        save_code_to_file(folder_name, file_name, code_name)
-        print(f"数据不存在")
-    else:
-        print(f"数据 {code_name} 已存在，不需要写入！")
-    stock_zh_a_tick_tx_js_df = ak.stock_zh_a_tick_tx_js(symbol="sz000002")
+    # data_exists = check_data_exists(folder_name, file_name, code_name)
+    # if not data_exists:
+    #     save_code_to_file(folder_name, file_name, code_name)
+    #     print(f"数据不存在")
+    # else:
+    #     print(f"数据 {code_name} 已存在，不需要写入！")
+    file_path = os.path.join(os.getcwd(), folder_name,f'{code_name}.parquet')
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    stock_zh_a_tick_tx_js_df = ak.stock_zh_a_tick_tx_js(code_name)
+    stock_zh_a_tick_tx_js_df.to_parquet(f'{code_name}.snappy.parquet', compression='snappy')
+    stock_zh_a_tick_tx_js_df.to_parquet(f'{code_name}.Gzip.parquet', compression='Gzip')
     print(stock_zh_a_tick_tx_js_df)
 if __name__ == "__main__":
    main()
